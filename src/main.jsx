ef6c94bb162c23a27583f6082d1e5bf287ef6dee
sergioiribe <sergio.iribec@gmail.com>
2024-05-21 17:28:01 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscardorPeliculas } from './BuscardorPeliculas.jsx'
+import './styles/buscador.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscardorPeliculas></BuscardorPeliculas>
+  </React.StrictMode>
+)
